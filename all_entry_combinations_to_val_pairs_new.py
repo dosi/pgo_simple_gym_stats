c585baf811169167453e7b61a9d7d319019b5701
Kalle Karhu <kalle.karhu@iki.fi>
2016-07-30 15:53:05 +0300

    added script for new data and new data after move update

---
+++ all_entry_combinations_to_val_pairs_new.py
@@ -0,0 +1,44 @@
+import sys
+import json
+from pprint import pprint
+
+data_mtype_fn = 'json_data/data_mtype.json'
+data_ptype_fn = 'json_data/data_ptype.json'
+
+with open(data_mtype_fn) as mfile:
+  mdata = json.load(mfile)
+with open(data_ptype_fn) as pfile:
+  pdata = json.load(pfile)
+
+if len(sys.argv) < 2:
+  print "usage: " + sys.argv[0] + " fileout"
+  sys.exit()
+out_fn=sys.argv[1]
+outfile = open(out_fn, 'w')
+
+print("len(mdata):" + str(len(mdata)))
+
+for pentry in pdata:
+  #for mkey in pentry["Quick Attacks"]:
+  for mkey in pentry["Quick Moves"]:
+    for mentry in mdata:
+      if mentry["ID"] == mkey and mentry.get("Power") and mentry.get("Duration (ms)"):
+        pwr = mentry["Power"]
+        dur = mentry["Duration (ms)"]/1000.0
+        power_per_sec = pwr/dur
+        multip = 1.0
+        for ptype in pentry["Types"]:
+          if ptype == mentry["Type"]:
+            multip = 1.25
+        power_per_sec = power_per_sec*multip
+
+        att_goodness = pentry["Base Attack"]*power_per_sec/100.0
+        def_goodness = pentry["Base Defense"]*pentry["Base Stamina"]/1000.0
+        print(str(pentry["PkMn"]))
+        print(str(pentry["Name"]))
+        print(str(mentry["Name"]))
+        print(str(att_goodness))
+        print(str(def_goodness))
+        lineOut = str(pentry["PkMn"]) + "," + str(pentry["Name"]) + "," + str(mentry["Name"]) + "," + str(att_goodness) + "," + str(def_goodness) + "\n"
+        outfile.write(lineOut)
+
