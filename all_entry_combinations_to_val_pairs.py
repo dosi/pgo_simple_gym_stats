30c8f1eeb1f4fe32057cbefbe9185ea2889fd9e2
Kalle Karhu <kalle.karhu@iki.fi>
2016-07-31 08:55:18 +0300

    added total numbers for equal IV% from 0 to 100 on 10% steps

--- all_entry_combinations_to_val_pairs.py
+++ all_entry_combinations_to_val_pairs.py
@@ -18,7 +18,7 @@ outfile = open(out_fn, 'w')
 
 print("len(mdata):" + str(len(mdata)))
 
-lineOut = "PkMn, PokeName, Quick Move Name, Charge Move Name, Attack, Defense, Att*Def, Should use charge\n"
+lineOut = "PkMn, PokeName, Quick Move Name, Charge Move Name, Attack, Defense, Att*Def, Should use charge, Base Attack, Base Defense, Base Stamina, Quick Move PPS, Charge Move PPS, Charge PPS Active Fraction, Total PPS, IV10%, IV20%, IV30%, IV40%, IV50%, IV60%, IV70%, IV80%, IV90%, IV100%\n"
 outfile.write(lineOut)
 
 for pentry in pdata:
@@ -65,12 +65,22 @@ for pentry in pdata:
                 charge_dps_active_fraction = charge_dur/(charge_dur + quick_move_active)
                 print(str(charge_dps_active_fraction))
                 total_power_per_sec = charge_dps_active_fraction*charge_power_per_sec + (1.0 - charge_dps_active_fraction)*power_per_sec
-              att_goodness = pentry["Base Attack"]*total_power_per_sec/100.0
-              def_goodness = pentry["Base Defense"]*pentry["Base Stamina"]/1000.0
+              att_goodness = [0.0]*11
+              def_goodness = [0.0]*11
+              goodness = [0.0]*11
+              iv_bonus_per_i = 1.5
+              for i in range(0,11):
+                iv_bonus = iv_bonus_per_i*i
+                att_goodness[i] = (pentry["Base Attack"]+iv_bonus)*total_power_per_sec/100.0
+                def_goodness[i] = (pentry["Base Defense"]+iv_bonus)*(pentry["Base Stamina"]+iv_bonus)/1000.0
+                goodness[i] = att_goodness[i]*def_goodness[i]
 
               print(str(att_goodness))
               print(str(def_goodness))
-              lineOut = str(pentry["PkMn"]) + "," + str(pentry["Name"]) + "," + str(mentry["Name"]) + "," + str(charge_move_entry["Name"]) + "," + str(att_goodness) + "," + str(def_goodness) + "," + str(att_goodness*def_goodness) + "," + str(should_use_charge) + "\n"
+              lineOut = str(pentry["PkMn"]) + "," + str(pentry["Name"]) + "," + str(mentry["Name"]) + "," + str(charge_move_entry["Name"]) + "," + str(att_goodness[0]) + "," + str(def_goodness[0]) + "," + str(goodness[0]) + "," + str(should_use_charge) + "," + str(pentry["Base Attack"]) + "," + str(pentry["Base Defense"]) + "," + str(pentry["Base Stamina"]) + "," + str(power_per_sec) + "," + str(charge_power_per_sec) + "," + str(charge_dps_active_fraction) + "," + str(total_power_per_sec)
+              for i in range(1,11):
+                lineOut = lineOut + "," + str(goodness[i])
+              lineOut = lineOut + "\n"
               outfile.write(lineOut)
 
  
